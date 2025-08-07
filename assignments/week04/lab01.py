0a4225fa7c886c9dcdd7941b2183aae21250202e
Boonchoo Jitnupong <Kannika.mi@ku.th>
2025-08-07 15:16:22 +0700

    week04

---
+++ assignments/week04/lab01.py
@@ -0,0 +1,61 @@
+"""
+Personal Information Manager
+ 
+#Create a tuple to store a person's basic info: (name, age, city, country)
+#Create a list to store their hobbies
+ 
+Allow the user to:
+ 
+Display all information
+Add new hobbies
+Remove hobbies
+Update age (by creating a new tuple)
+ 
+"""
+ 
+# Complete this program
+def personal_info_manager():
+    # Create initial person tuple
+    person = ("Kaowfang", 19, "Chiang Rai", "Thailand")  # name, age, city, country
+    hobbies = []
+ 
+ 
+    # Your code here
+    while True:
+ 
+        print("1.display into")
+        print("2.add hobby")
+        print("3.Remove hobby")
+        print("4.Edit age")
+        print('5.Exit')
+        choice = input("What do you want to do :" )
+ 
+        if choice == "1":
+            print("Name : ", person[0])
+            print("Age : ", person[1])
+            print("City : ", person[2])
+            print("conntry ; ", person[3])
+            print("Hobbies : ", hobbies)
+ 
+        elif choice == "2":
+            hobby = input("insert new hobby : ")
+            hobbies.append(hobby)
+       
+        elif choice == "3":
+            del hobbies[0]
+       
+        elif choice == "4":
+            age = input("insert new age : ")
+            person_list = list(person) #["Kaowfang", 19, "Chiang Rai", "Thailand"]
+            person_list[1] = age
+            person = tuple(person_list)
+ 
+        elif choice == "5":
+            break
+       
+ 
+if __name__ == "__main__":
+    personal_info_manager()
+ 
+ 
+ 
